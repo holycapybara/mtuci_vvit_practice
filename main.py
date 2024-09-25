727163bd185ebfabd7cb984662a76f3027909769
holycapybara <grig.bel777@gmail.com>
2024-09-25 22:33:51 +0300

    finished lab3

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import lab1
 import lab2
+import lab3
 
 
 def main():
-    lab1.task1_2()
+    while True:
+        task = input(f'Select a task:\n'
+                         f'(1) - Reading a file\n'
+                         f'(2) - Writing to a file\n'
+                         f'(3) - Exit\n')
+        if task not in {"1", "2", "3"}:
+            print("Please enter a correct option")
+            continue
+        task = int(task)
+        if task == 1:
+            lab3.task_1()
+            print()
+        if task == 2:
+            lab3.task_2()
+        if task == 3:
+            break
+
+
 
 if __name__ == "__main__":
     main()
